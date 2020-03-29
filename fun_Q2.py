37fb808d08617992611154bb6545e78fc2fe8fd8
pastesrh <46579091+pastesrh@users.noreply.github.com>
2020-03-29 01:49:47 -0400

    Add files via upload
    nouvelle version de la function crank nickelson, il manque la frontière efficiente et il y a certain petit trucs que je suis pas certaine. La fonction n'a pas été testé, elle ne marche donc probablement pas, mais elle semble faire du sens.

--- fun_Q2.py
+++ fun_Q2.py
@@ -12,9 +12,9 @@ def CN(K,r,T,vol,n_a,n_t,type,barrier = None):
     w = 1.2
     v1 = delta_t/delta_s**2
     v2 = delta_t/delta_s
-    r = 0.05
-    a = r
-    c = -0.05
+
+    a = vol ## a verifier
+    c = -r
 
 
     if barrier != None:
@@ -23,10 +23,10 @@ def CN(K,r,T,vol,n_a,n_t,type,barrier = None):
         S_max = 200
 
 
-    grid = np.zeros((n_a,n_t))
+    grid = np.zeros((n_a+1,n_t+1))
 
     # Boundary a optimiser 
-    for i in range(n_t):
+    for i in range(n_t+1):
 
         k = T - i
 
@@ -47,30 +47,33 @@ def CN(K,r,T,vol,n_a,n_t,type,barrier = None):
 
     grid[n_t,:] = max(range(0,S_max,delta_a)-K,0)
 
-    vect_gk = max(range(0,S_max,delta_a)-K,0)
-    for i in range(1,n_t-1,1):
+    for i in range(1,n_t,1):
+        vect_gk = grid[n_t-i-1,:]
         eps = 1
-        gu = max(range(0,S_max,delta_a)-K,0)
+        gu = vect_gk 
         while  eps > 0.00001:
-            vect_gk1 = np.zeros(n_a)
-            vect_gk1[0] = grid[0][n_t-i]
-            vect_gk1[S_max] = grid[S_max][n_t-i]
-            gu_1 = np.zeros(n_a+1)
+            gu_1 = np.zeros(n_a+1)  ## k+1
             gu_1[0] = grid[0][n_t-i]
             gu_1[S_max] = grid[S_max][n_t-i] 
-            for j in range(1,n_a-1,1):
-                b = (n_a-j)*S_max*r
+            for j in range(1,n_a,1):
+                bk = (n_a-j)*S_max*r     ############ A verifier
+                # bk_1 = (n_a-j)*S_max*r
                 Ak = v1*a-0.5*v2*b
                 Bk = -2*v1*a+delta_t*c
                 Ck = v1*a+0.5*v2*b
-                alphak_1 = -v1*
-                betak_1 = 
-                lambdak_1 = 
-                h_k = -A*gu_1[j-1]-B*gu_1[j]-C*gu_1[j+1]
-                gu_1[j] = gu[j] + (w/beta) 
-        
-            eps = 
+                alphak_1 = -v1*a+0.5*v2*bk
+                betak_1 = 2*v1*a-delta_t*c
+                lambdak_1 = -v1*a-0.5*v2*bk
+                h_k = -Ak*vect_gk[j-1]-Bk*vect_gk[j]-Ck*vect_gk[j+1]
+                if type == "european call" or type == "european put":
+                    gu_1[j] = gu[j] + (w/betak_1)*(h_k-alphak_1*gu_1[j-1]-betak_1*gu[j]-lambdak_1*gu[j+1]) 
+                else : 
+                    gu_1[j] = max(gu[j] + (w/betak_1)*(h_k-alphak_1*gu_1[j-1]-betak_1*gu[j]-lambdak_1*gu[j+1]),(n_a-j)*S_max-K)  
+            eps = np.sqrt(np.dot((gu-gu_1)[1:-1],(gu-gu_1)[1:-1]))          ############### A verifier
             gu = gu_1
+        grid[n_t-i,:] = gu_1
 
-        gi = 
-        vect_gk = vect_gk1
+    # frontier efficiente    
+    frontier = 
+
+    return grid, frontier
